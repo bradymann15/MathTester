bb6fcd52a9d6d6a610dc168586c8798d5dbe5fb1
Brady Mann <Football3320>
2019-02-04 14:01:54 -0500

    Declared firstNumber and secondNumber as integers

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,6 +3,8 @@ public class MathTester {
     public static void main(String[] args){
         String firstWord;
         String secondWord;
+        int firstNumber,secondNumber; 
+
 
         firstWord = "dog";
         secondWord = "house";
