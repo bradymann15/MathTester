fb4fb78a22983613095f5d851c58bbf9c8bc95c3
Brady Mann <Football3320>
2019-02-05 14:00:05 -0500

    made changes to get ready for screencast

--- src/MathTester.java
+++ src/MathTester.java
@@ -12,12 +12,12 @@ public class MathTester {
         secondNumber = 10;
 
         System.out.println(firstWord+secondWord);
-        System.out.println(firstWord+(firstNumber+secondNumber));
-        System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
-        System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
-        System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
-        System.out.println("firstNumber x secondNumber is "+ (firstNumber*secondNumber));
-        System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
+        //System.out.println(firstWord+firstNumber);
+        //System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+        //System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+        //System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
+        //System.out.println("firstNumber x secondNumber is "+ (firstNumber*secondNumber));
+        //System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
 
 
     }
